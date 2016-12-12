596ccd4a1c2bbbe5383a8120b767df0c49e433ed
kevin.lefevre <kevin.lefevre@gmail.com>
2016-12-12 17:55:34 -0500

    midtermfirstdeploy

---
+++ public/main.js
@@ -0,0 +1,105 @@
+//factory of dummy accounts, maybe 4-5 people
+    //login ==opens model with sign in info
+// make them find each other on google map (add marker on map) or at least how far away they are from each other [Bob is 4 miles away] list 
+//add SoundCloud link
+//add Eventful API for concerts search favorite band
+//
+
+
+angular.module('JamSession', ['ngRoute']);
+
+// In order to use ngRoute as our router, we have to define what routes it should be looking for
+angular.module('JamSession')
+  .config(Router);
+
+Router.$inject = ['$routeProvider']; // Inject component from ngRoute
+
+
+
+
+function Router ($routeProvider) {
+  
+  $routeProvider
+    .when('/', {
+      templateUrl   : '/home.html',// URL on your server that leads to an html file to be used for this page
+      controller    : 'jamTime', // String name of the angular controller you would like to use on this page
+      controllerAs  : 'jCtrl'  // Object name for `this` in your html from the specified controller 
+    })
+ 
+    .when('/profilepage', {
+      templateUrl   : '/profilepage.html',
+    })
+    .when('/connect', {
+      templateUrl   : '/connect.html',
+    })
+    .when('/signup', {
+      templateUrl   : '/signup.html',
+    })
+//     .when('/login', {
+//       templateUrl   : '/login.html',
+//     })
+}
+
+
+
+
+
+
+
+
+// User Factory here
+
+angular.module("JamSession")
+  .controller("jamTime", userCtrl)
+
+userCtrl.$inject = ["userFactory", "$location"] //factory, $location
+
+function userCtrl (userFactory, $location) {
+  var jCtrl = this;
+   jCtrl.userInfo= userFactory.userdata;
+
+    
+    
+    //    
+   jCtrl.saveUser = function (){
+       $location.url("/profile");
+   }
+}
+
+
+
+
+// ----------FACTORY------------
+
+
+angular.module("JamSession")
+  .factory("userFactory", mainuserFactory);
+
+
+
+function mainuserFactory () {
+  var userInfo = [
+    {
+      fn : "",
+      ln : "",
+      email: "",
+      instrument: "",
+      zipcode: "",
+      host: "",
+      othersplay: "",
+      genre1: "",
+      genre2: "",
+      genre3: "",
+    },
+
+  ];
+  
+  //   ALL FACTORIES MUST HAVE A RETURN STATEMENT!
+  return {
+    userdata : userInfo
+  }
+  
+}
+
+
+
